4bd81abe88c85336e2ce732a757d8c97d4ba3941
Divya Verma <div@Divyas-MacBook-Air.local>
2024-07-23 16:40:56 +0530

    Initial Changes

---
+++ Meditation/Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Divya Verma on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
